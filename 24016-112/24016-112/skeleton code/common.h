56e0159897f5d950f33f24466241ffc9860bdefe
KIRAN DEEPTHI.E <126153047+EGAVENKATAKIRANDEEPTHI@users.noreply.github.com>
2024-11-28 12:36:15 +0530

    Add files via upload

---
+++ 24016-112/24016-112/skeleton code/common.h
@@ -0,0 +1,11 @@
+/* NAME : KIRAN DEEPTHI.E
+   DATE : 25-09-2024
+   DESCRIPTION : common.h  */
+
+#ifndef COMMON_H
+#define COMMON_H
+
+/* Magic string to identify whether stegged or not */
+#define MAGIC_STRING "#*"
+
+#endif
